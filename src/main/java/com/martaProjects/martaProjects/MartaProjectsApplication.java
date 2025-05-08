da05bb57cdbf3b9214ab0c34663f385646a55fb5
pmorenateide <pmorena@teideformacion.com>
2025-05-08 10:33:06 +0200

    Add files via upload

---
+++ src/main/java/com/martaProjects/martaProjects/MartaProjectsApplication.java
@@ -0,0 +1,13 @@
+package com.martaProjects.martaProjects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartaProjectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MartaProjectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/martaProjects/martaProjects/MartaProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martaProjects.martaProjects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MartaProjectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
